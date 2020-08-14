65d2f3acbc0e0d3452e932acc92586fe157a8a29
mugen 430 <acampado.maverick@gmail.com>
2020-08-14 13:35:38 -0500

    added portions of JS file

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-//  $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }) 
+var authKey = "Place-holder";
+
+
+var queryTerm = "";
+var numResults = 0;
+var startYear = 0;
+var endYear = 0;
+
+
+var queryURLBase = "http://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+
+
+
+function runQuery(numArticles, queryURL) {
+
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(NYTData) {
+
+    
+    console.log("------------------");
+    console.log(queryURL);
+    console.log("------------------");
+    console.log(numArticles);
+    console.log(NYTData);
+
+   
+    $("#well-section").empty();
